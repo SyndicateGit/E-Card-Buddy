81280f3d4353ea29a6fad862f2e5e6ce4f123e32
Raymond Zeng <shilei.zeng98@outlook.com>
2024-06-17 22:51:34 -0400

    Initialized Next app, set up folder structure, tailwind and daisyUI config, and temporary metadata icon.

---
+++ src/app/(pages)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
+
+export default Dashboard;
